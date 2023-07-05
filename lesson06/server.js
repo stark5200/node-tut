0b071c9a67e2ba9dbac8ddad418cf6506f33621e
stark5200 <a.taha5200@gmail.com>
2023-07-05 19:40:04 +0200

    end of lesson 06

--- lesson06/server.js
+++ lesson06/server.js
@@ -4,8 +4,26 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('^/$|/index(.html)?', (req, res) => {
+  //res.sendFile('./views/index.html', { root: __dirname})
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
+
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect(301, '/new-page.html'); //302 response code by default
+});
+
+// Route handlers
+
+
+app.get('/*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
