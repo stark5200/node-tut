3c921052f2041d16f730c38e8f2ae0f9dba088d5
stark5200 <a.taha5200@gmail.com>
2023-07-08 03:56:56 +0200

    end of lesson 07

--- lesson06/server.js
+++ lesson06/server.js
@@ -3,13 +3,26 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors'); 
 const app = express();
-const { logger, logEvents } = require('./middleware/logEvents')
+const { logger, logEvents } = require('./middleware/logEvents');
+const  errorHandler  = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
 app.use(logger); 
+
 // Cross Origin Resource Sharing
-app.use(cors());
+const whitelist = ['https://www.site.com', 'http://127.0.0.1:5500', 'http://localhost:3500', 'https://www.google.com']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, 
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // built in middleware to handle urlencoded data
 // in other words: form data:
@@ -37,10 +50,17 @@ app.get('/old-page(.html)?', (req, res) => {
 });
 
 // Route handlers
-
-
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type('txt').send("404 not Found");
+  }
 })
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
