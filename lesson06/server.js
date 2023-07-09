c480bba488e82c1db54f1837a1e90cb6e82a7ab6
stark5200 <a.taha5200@gmail.com>
2023-07-09 06:32:25 +0200

    End of lesson 08

--- lesson06/server.js
+++ lesson06/server.js
@@ -33,21 +33,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve static files 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-
-app.get('^/$|/index(.html)?', (req, res) => {
-  //res.sendFile('./views/index.html', { root: __dirname})
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, '/new-page.html'); //302 response code by default
-});
+// routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir')); 
+app.use('/employees', require('./routes/api/employees')); 
 
 // Route handlers
 app.all('*', (req, res) => {
